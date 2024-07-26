a414c7df70ec98a001e359db160fe246746b7bdc
seeungirl <seeungirl99@gmail.com>
2024-07-26 11:47:28 +0900

    web_controller

--- shopping_mall/src/main/java/com/navershop/www/web_controller.java
+++ shopping_mall/src/main/java/com/navershop/www/web_controller.java
@@ -3,8 +3,13 @@ package com.navershop.www;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_controller {
 	PrintWriter pw = null;
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		return null;
+	}
 }
